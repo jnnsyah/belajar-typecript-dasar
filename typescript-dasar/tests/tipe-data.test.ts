20860d136217d8a9a4e385aea756bf342692115e
Jiansyah <jiansyah1248@gmail.com>
2025-08-08 21:00:23 +0700

    Tipe data primitif test

---
+++ typescript-dasar/tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data Type', function () {
+  it('should must declare', function () {
+    const name: string = "Rama Jiansyah";
+    const balance: number = 66666;
+    const isVip: boolean = true;
+
+    console.info(name);
+    console.info(balance);
+    console.info(isVip);
+  })
+})
